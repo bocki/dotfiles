b006f30f62c0da70a08c88ab41ad85b624cbb18e
Florian <bocki@users.noreply.github.com>
2016-02-09 18:58:44 +0100

    added script to toggle touchpad

---
+++ bin/touchpad_toggle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ID="SynPS/2 Synaptics TouchPad"
+declare -i STATE
+STATE=`xinput list-props "$ID"|grep 'Device Enabled'|awk '{print $4}'`
+if [ $STATE -eq 1 ]
+then
+    xinput disable "$ID"
+    # echo "Touchpad disabled."
+    # notify-send 'Touchpad' 'Disabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
+else
+    xinput enable "$ID"
+    # echo "Touchpad enabled."
+    # notify-send 'Touchpad' 'Enabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
+fi
